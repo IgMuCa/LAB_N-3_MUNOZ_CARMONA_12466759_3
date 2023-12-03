4cd5bdd8c79309219d0edfbd91e28d0ee4296c8a
Ignacio Munoz <ignacio.munoz.ca@usach.cl>
2023-12-03 18:15:03 -0300

    Sexto Commit

--- LAB_3_MUNOZ_CARMONA_12466759_3/src/main/java/org/example/Main.java
+++ LAB_3_MUNOZ_CARMONA_12466759_3/src/main/java/org/example/Main.java
@@ -4,8 +4,29 @@ import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //METODO PARA SCANNEAR
-        make_user();
+        System_124667593_MUNOZ_CARMONA sistemaOrigen;
+
+        Chatbot_124667593_MUNOZ_CARMONA chatbotOrigen;
+
+        
+        // EL DIALOGO COMIENZA POR ELEGIR QUE HACER
+        // a.-Ingresar un usuario
+        // b.-Ingresar un Chatbot
+        // c.-Interactuar con el sistema
+        // d.-Salir
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Que desaes hace?");
+        System.out.println("a.-Ingresar un usuario (Marca 1)");
+        System.out.println("b.-Ingresar un Chatbot (Marca 2)");
+        System.out.println("c.-Interactuar con el sistema (Marca 3)");
+        System.out.println("d.-Salir (Marca 4)");
+        int altEntrada= scanner.nextInt(); //
+
+
+
+
+
 
 
 
@@ -29,19 +50,15 @@ public class Main {
         System.out.println("(1) Administrador");
         System.out.println("(2) Usuario Normal");
         String userRol= scanner.nextLine();
-
     }
 
-    public static void enter_user() {
-
-
-    }
 
     //Crea un chatbot
     public static Chatbot_124667593_MUNOZ_CARMONA make_chatbot() {
         List<Flow_124667593_MUNOZ_CARMONA> flujos = new ArrayList<>();
         Flow_124667593_MUNOZ_CARMONA flujo;
-        //Se ingresa por consola flujo
+
+        //Se ingresa por consola el chatbot:
         Scanner scanner = new Scanner (System.in);
         System.out.println("Ingrese ID del chatbot");
         int id_chatbot= scanner.nextInt();
@@ -51,6 +68,10 @@ public class Main {
         String msg_chatbot= scanner.nextLine();
         System.out.println("Ingrese startFlowID");
         int startFlowID_cb= scanner.nextInt();
+
+        //Se crea el chatbot nuevo al que se le agregaran los flujos:
+        Chatbot_124667593_MUNOZ_CARMONA nuevoCb = new Chatbot_124667593_MUNOZ_CARMONA(id_chatbot, name_chatbot, msg_chatbot, startFlowID_cb, flujos);
+
         //Abajo: while para llenar la flujos
         System.out.println("Deseas ingresar un flujos?  Si (Marca 1) / No (Marca 2) ");
         int var1 = scanner.nextInt();
@@ -59,46 +80,48 @@ public class Main {
             flujos.add(flujo);
             System.out.println("Deseas ingresar otro flujo?  Si (Marca 1) / No (Marca 2) ");
             int var10 = scanner.nextInt();
-            if (var10==1) {
-                var1=1;
-            }
-            else { var1=2;}
+            var1 = var10;
         }
-        return new Chatbot_124667593_MUNOZ_CARMONA(id_chatbot, name_chatbot, msg_chatbot, startFlowID_cb, flujos);
+        return nuevoCb;
     }
+
 
 
     //Crea un flujo
     public static Flow_124667593_MUNOZ_CARMONA make_flow() {
         List<Option_124667593_MUNOZ_CARMONA> opciones = new ArrayList<>();
-        Option_124667593_MUNOZ_CARMONA op;
-        //Se ingresa por consola flujo
+        Option_124667593_MUNOZ_CARMONA opcionNueva;
+
+        //Se ingresa por consola flujo:
         Scanner scanner = new Scanner (System.in);
         System.out.println("Ingrese Id del Flow");
         int id_flow= scanner.nextInt();
         System.out.println("Ingrese mensaje");
         String msg_flow= scanner.nextLine();
-        //Abajo: while para llenar la opciones
+
+        //Se crea el flujo nuevo al que se le agregaran las opciones:
+        Flow_124667593_MUNOZ_CARMONA nuevoFlujo = new Flow_124667593_MUNOZ_CARMONA(id_flow, msg_flow, opciones);
+
+        //Abajo: while para llenar la opciones:
         System.out.println("Deseas ingresar una opcion?  Si (Marca 1) / No (Marca 2) ");
         int var2 = scanner.nextInt();
         while (var2 == 1){
-            op=make_option();
-            opciones.add(op);
+            opcionNueva = make_option();
+            nuevoFlujo.addOpcionToOptions(opcionNueva);
             System.out.println("Deseas ingresar otra opcion?  Si (Marca 1) / No (Marca 2) ");
             int var10 = scanner.nextInt();
-            if (var10==1) {
-                var2=1;
-            }
-            else { var2=2;}
+            var2 = var10;
         }
-        return new Flow_124667593_MUNOZ_CARMONA(id_flow, msg_flow, opciones);
+        return nuevoFlujo;
     }
+
 
 
     //Crea una opcion
     public static Option_124667593_MUNOZ_CARMONA make_option() {
         List<String> keywords = new ArrayList<>();
-        //Se ingresa por consola opcion
+
+        //Se ingresa por consola la opcion:
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese Code de la opcion");
         int code = scanner.nextInt();
@@ -108,6 +131,7 @@ public class Main {
         int codeLinkCb = scanner.nextInt();
         System.out.println("Ingrese el CodeLink del Flow asociado");
         int codeLinkFl = scanner.nextInt();
+
         //Abajo: while para llenar la keywords
         System.out.println("Deseas ingresar una keywords?  Si (Marca 1) / No (Marca 2) ");
         int var3 = scanner.nextInt();
@@ -117,10 +141,7 @@ public class Main {
             keywords.add(kw);
             System.out.println("Deseas ingresar otra keywords?  Si (Marca 1) / No (Marca 2) ");
             int var10 = scanner.nextInt();
-            if (var10==1) {
-                var3=1;
-            }
-            else { var3=2;}
+             var3=var10;
         }
         return new Option_124667593_MUNOZ_CARMONA(code, msg_opcion, codeLinkCb, codeLinkFl, keywords );
     }
